f1e8aebdff33867ab649e4349d838c6d9c131f45
smritikc <ismritikc1@gmail.com>
2026-02-12 11:39:21 +0545

    import update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './Home.css'
 import { Header } from '../../components/header/Header'
-import { ExploreMenu } from '../../components/exploreMenu/exploreMenu'
+import { ExploreMenu } from '../../components/exploreMenu/ExploreMenu'
 import { FoodDisplay } from '../../components/FoodDisplay/FoodDisplay'
 import { AppDownload } from '../../components/AppDownload/AppDownload'
 export const Home = () => {
